6bbc7566d086abe540cde4d8aa1ba1d4eac313b4
robo <robo2020@gmail.com>
2022-02-01 21:29:27 +0200

    bk

--- gz/plugins/VelodynePlugin/velodyne_plugin.cc
+++ gz/plugins/VelodynePlugin/velodyne_plugin.cc
@@ -24,34 +24,48 @@ namespace gazebo
             // Store the model pointer for convenience.
             this->model = _model;
             this->joint = _model->GetJoints()[0];
+            gzmsg << this->joint->GetName() << std::endl;
+            
             this->pid = common::PID(0.1, 0, 0);
             this->model->GetJointController()->SetVelocityPID(
                 this->joint->GetScopedName(), this->pid);
 
             // Default to zero velocity
-            double velocity = 10;
+            this->omega = -0.2;
 
             // Check that the velocity element exists, then read the value
             if (_sdf->HasElement("velocity"))
-                velocity = _sdf->Get<double>("velocity");
+                this->omega = _sdf->Get<double>("velocity");
 
-            this->SetVelocity(velocity);
+            this->SetVelocity(this->omega);
             this->node = transport::NodePtr(new transport::Node());
             this->node->Init(this->model->GetWorld()->Name());
 
             // Create a topic name
             std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
 
+            this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+                std::bind(&VelodynePlugin::OnUpdate, this));
             // Subscribe to the topic, and register a callback
             this->sub = this->node->Subscribe(topicName,
                                               &VelodynePlugin::OnMsg, this);
         }
 
     public:
+    void OnUpdate(){
+        gzmsg << this->joint->Position() << std::endl;
+        double angale = this->joint->Position();
+        if (abs(angale) >= 1.57){
+            this->dir *= -1;
+            this->SetVelocity(this->omega);
+        }
+        
+
+    }
         void SetVelocity(const double &_vel)
         {
             this->model->GetJointController()->SetVelocityTarget(
-                this->joint->GetScopedName(), _vel);
+                this->joint->GetScopedName(), _vel* this->dir);
         }
 
     private:
@@ -66,6 +80,9 @@ namespace gazebo
         physics::ModelPtr model;
         physics::JointPtr joint;
         common::PID pid;
+        event::ConnectionPtr updateConnection;
+        int dir=1;
+        double omega = 0;
     };
     GZ_REGISTER_MODEL_PLUGIN(VelodynePlugin)
 }
